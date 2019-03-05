dcef09cb757c959eb666137b159f92598eff9f7c
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2019-03-05 16:58:58 +0100

    divide y venceras

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,5 @@ I
 src/tema1/divisiones.java,5/c/5c37216b9e449f96d5cc30eee52f2de776e5383b
 K
 src/tema1/cosas_arrays.java,c/5/c57de7c0f90006d81386c4de9d933fce52e54e63
+Z
+*src/tema1/src/tema2/divide_y_venceras.java,9/a/9a781d78f0efb358fe3e0d780006d3a3068daa98
---
+++ src/tema1/src/tema2/divide_y_venceras.java
@@ -0,0 +1,87 @@
+package src.tema1.src.tema2;
+
+public class divide_y_venceras {
+
+    public static int maximoDyV(int[] array, int inicio, int fin) {
+        if (inicio == fin) {
+            return array[inicio];
+        } else {
+            int mitad = (inicio + fin) / 2;
+            int maximoIzq = maximoDyV(array, inicio, mitad);
+            int maximoDrcha = maximoDyV(array, mitad + 1, fin);
+            if (maximoIzq > maximoDrcha) {
+                return maximoIzq;
+            } else {
+                return maximoDrcha;
+            }
+        }
+    }
+
+    //la lista tiene que estar previamente ordenada
+    public static int busquedaBinaria(int[] array, int dato, int inicio, int fin) {
+        if (array[inicio] > dato || array[fin] < dato || array.length == 0)
+            return -1;
+        else {
+            if (inicio > fin) {
+                return -1;
+            } else {
+                int mitad = (inicio + fin) / 2;
+                if (array[mitad] == dato) {
+                    return mitad;
+                } else {
+                    if (array[mitad] > dato) {
+                        return busquedaBinaria(array, dato, inicio, mitad - 1);
+                    } else {
+                        return busquedaBinaria(array, dato, mitad + 1, fin);
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    public static void escribirArray(int[] array, int inicio, int fin) {
+        if (inicio == fin) {
+            System.out.println(array[inicio]);
+        } else {
+            int mitad = (inicio + fin) / 2;
+            escribirArray(array, inicio, mitad);
+            escribirArray(array, mitad + 1, fin);
+        }
+    }
+
+    public static boolean compararArrays(int[] array1, int[] array2, int inicio, int fin) {
+        if (array1.length == array2.length) {
+            int mitad = (inicio + fin) / 2;
+            if (array1[mitad] != array2[mitad]) {
+                return false;
+            } else {
+                if (inicio > fin) {
+                    return true;
+                } else {
+                    return compararArrays(array1, array2, inicio, mitad - 1)
+                            && compararArrays(array1, array2, mitad + 1, fin);
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] lista = {10, 21, 42, 63, 84, 55, 46, 7};
+        int[] lista2 = {21, 32, 43, 10, 66, 90, 23};
+        int[] lista3 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] lista4 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println(maximoDyV(lista, 0, lista.length - 1));
+        System.out.println(maximoDyV(lista2, 0, lista2.length - 1));
+        System.out.println(busquedaBinaria(lista3, 10, 0, lista3.length - 1));
+        escribirArray(lista3, 0, lista3.length - 1);
+
+        System.out.println(compararArrays(lista3, lista4, 0, lista3.length - 1));
+
+    }
+}
